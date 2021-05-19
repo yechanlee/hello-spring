c40a5f66cc14029aaa19010a4ce86dbb1f3631d9
firecare@naver.com <firecare@naver.com>
2021-05-20 00:34:33 +0900

    2021.05.20 spring home

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,15 +1,17 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-    @Override
+    //@Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(),member);
